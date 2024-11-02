7e6574cac81341091041d7a2712db6ed7b6d5127
muriarty1983 <murateker9166@gmail.com>
2024-11-02 12:37:56 +0300

    V 1.2.1 - typo fix

--- test_logreg1.py
+++ test_logreg1.py
@@ -38,5 +38,5 @@ if __name__ == "__main__":
     plt.yticks(())
 
     plt.savefig("grafik.png")
-    # plt.show() çalışmadı, bende proje dizininde foto olarak olustutturdum.
+    # plt.show() calısmadı, bende proje dizininde foto olarak olustutturdum.
     print("plt.show() çalismadi, bende proje dizininde foto olarak olustutturdum (grafik.png).")
